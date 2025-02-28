e5f39c1f86911b998ca8b742af256e8d7309f334
kamrul <bsse1418@iit.du.ac.bd>
2025-03-01 00:09:39 +0600

    strange behavior

--- HatBazar/Backend/database.py
+++ HatBazar/Backend/database.py
@@ -5,7 +5,8 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///default.db")
+
 
 engine = create_engine(DATABASE_URL, echo=True)
 
