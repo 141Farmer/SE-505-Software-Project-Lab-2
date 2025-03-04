01f53ef8ace9fc74c6dbef05c08d6d43dd733fde
kamrul <bsse1418@iit.du.ac.bd>
2025-03-05 01:19:12 +0600

    maybe

--- HatBazar/Backend/main.py
+++ HatBazar/Backend/main.py
@@ -30,13 +30,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+user=User()
+
 Database.create_db_and_tables()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
-user = User()
-
-
+app.include_router(community_router)
+app.include_router(post_router)
 app.include_router(user_router, prefix="", tags=["User"])
 app.include_router(marketplace_router, prefix="/marketplace", tags=["Market"])
 
@@ -148,6 +149,3 @@ def get_product(product_id: int):
             production_procedure=product.production_procedure
         )
 
-app.include_router(community_router)
-
-app.include_router(post_router)
